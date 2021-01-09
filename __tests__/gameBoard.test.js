9a6ca9a784effd72b34a620deea8a56eba3e5e3d
Cloudbop <boppincolin@gmail.com>
2021-01-09 17:20:14 +0000

    updated tests to check for special chars

--- __tests__/gameBoard.test.js
+++ __tests__/gameBoard.test.js
@@ -35,13 +35,13 @@ describe('Tests for the Game', () => {
   })
 
 
-  test('should set cells from 2d array', ()=>{
+  test('should set cells from 2d array, inc special chars', ()=>{
 
     const initialState = [new Array(3),new Array(3)]
     const currentGame = new GameBoard(initialState)
     //
     currentGame.stateStateFromArray([
-      ["0", " ", " " ," "],
+      ["0", "-1", "-2" ,"-3"],
       [" ", "8", " " ,"4"]
     ])
 
@@ -49,10 +49,17 @@ describe('Tests for the Game', () => {
       cell1: currentGame.cellContents(0,0),
       cell2: currentGame.cellContents(1,1),
       cell3: currentGame.cellContents(3,1),
+
+      cell4: currentGame.cellContents(1,0),
+      cell5: currentGame.cellContents(2,0),
+      cell6: currentGame.cellContents(3,0),
     }
 
     expect(result.cell1).toEqual(0)
     expect(result.cell2).toEqual(8)
     expect(result.cell3).toEqual(4)
+    expect(result.cell4).toEqual("")
+    expect(result.cell5).toEqual("!")
+    expect(result.cell6).toEqual("x")
   })
 })
